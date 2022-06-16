59c39630355e481912e91fbf5a37ff55e5dd7e68
GabrielCCouto <gabrihcosta@gmail.com>
2022-06-15 22:02:17 -0300

    main code

--- nanoleaf/src/main.cpp
+++ nanoleaf/src/main.cpp
@@ -1,42 +1,5 @@
 #include <Arduino.h>
-#include <FastLED.h>
-
-#define NUM_LEDS_FITA 40
-#define PINO_FITA 12
-CRGB fita[NUM_LEDS_FITA];
-
-void raioArcoiris() {
-  static uint8_t hue = 0;
-  int dir1 = 0;
-  int i1 = 0;
-
-  for (int i = 0; i < 2000; i++) {
-    fita[i1] = CHSV(hue++, 255, 255);
-
-    FastLED.show();
-    for (int i = 0; i < NUM_LEDS_FITA; i++) {
-      fita[i].nscale8(250);
-    }
-
-    if (dir1 == 0) {
-      i1++;
-    } else {
-      i1--;
-    }
-    if (i1 == NUM_LEDS_FITA) {
-      dir1 = 1;
-    } else if ( i1 == 0) {
-      dir1 = 0;
-    }
-    delay(10);
-  }
-
-}
-
-void fillSolidColor(CRGB selcor) {
-  fill_solid(fita, NUM_LEDS_FITA, selcor);
-  FastLED.show();
-}
+#include "effect.h"
 
 void setup() {
   FastLED.addLeds<WS2812B, PINO_FITA, GRB>(fita, NUM_LEDS_FITA);
@@ -47,5 +10,5 @@ void setup() {
 
 void loop() {
   raioArcoiris();
-  fillSolidColor(CRGB::Black);
+  cometa(150);
 }

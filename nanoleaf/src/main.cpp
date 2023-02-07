9f0a5b0e84cfb52ecc7004f5ba819d21c556400d
Gabrielhcouto <gabriel@sumersoft.com>
2023-02-06 21:21:38 -0300

    Correção Bug "travando"

--- nanoleaf/src/main.cpp
+++ nanoleaf/src/main.cpp
@@ -1,22 +1,40 @@
-#include <Arduino.h>
 #include <FastLED.h>
+#include <EEPROM.h>
 #include "effects.h"
 
 #define touch 2
 
-int contador = 0;
+int contador;
 int effectCount = 0;
+int memory_adress = 0;
+int effect_counter = 0;
+int led_off = 0;
 
 void(* resetFunc) (void) = 0;
 
 void pinTouch (){
-  contador++;
-  loop();
+  update_effect();
+  resetFunc();
+}
+
+void update_effect(){
+  Serial.println("Effect Updated...");
+  EEPROM.update(memory_adress, (effect_counter + 1));
 }
 
 void setup() {
   pinMode(touch, INPUT);
   Serial.begin(115200);
+
+  EEPROM.begin();
+  delay(300);
+  if (digitalRead(touch) == HIGH){
+    EEPROM.update(memory_adress, led_off);
+    resetFunc();
+  }
+  effect_counter = EEPROM.read(memory_adress);
+  Serial.print("Effect Value: ");
+  Serial.println(effect_counter);
 
   FastLED.addLeds<WS2812B, PINO_FITA, GRB>(fita, NUM_LEDS_FITA);
   FastLED.setBrightness(100);
@@ -25,17 +43,17 @@ void setup() {
 }
 
 void loop() {
-  if (contador == 0){
+  if (effect_counter == 0){
     fillSolidColor(CRGB::Black);
   }
-  else if (contador == 1){
+  else if (effect_counter == 1){
     uint8_t color = 125;
     cometa(color);
   }
-  else if (contador == 2){
+  else if (effect_counter == 2){
     raioArcoirisCircular();
   }
-  else if (contador == 3){
+  else if (effect_counter == 3){
     effectCount++;
     gradienteOndas(effectCount);
     if (effectCount == 10)
@@ -43,35 +61,36 @@ void loop() {
       effectCount = 0;
     }
   }
-  else if (contador == 4){
+  else if (effect_counter == 4){
     respiracao(255);
   }
-  else if (contador == 5){
+  else if (effect_counter == 5){
     gradienteMovendo();
   }
-  else if (contador == 6){
+  else if (effect_counter == 6){
     PixelsAleatorios();
   }
-  else if (contador == 7){
+  else if (effect_counter == 7){
     bolasColoridas();
   }
-  else if (contador == 8){
+  else if (effect_counter == 8){
     explosao();
   }
-  else if (contador == 9){
+  else if (effect_counter == 9){
     circular(125);
   }
-    else if (contador == 10){
-    raioArcoiris();
+    else if (effect_counter == 10){
+      raioArcoiris();
   }
-  else if (contador == 11){
+  else if (effect_counter == 11){
     fillSolidColor(CRGB::Blue);
   }
-  else if (contador == 12){
+  else if (effect_counter == 12){
     fillSolidColor(CRGB::DarkCyan);
   }
-  else if (contador == 13)
+  else if (effect_counter == 13)
   {
+    EEPROM.update(memory_adress, 0);
     resetFunc();
   }
 }

566566831d8d3351c921a3f651f9b7c731b74c5a
GabrielCCouto <gabrihcosta@gmail.com>
2025-05-22 20:06:55 -0300

    Adiciona o código de comunicação do sensor as5600

--- Robotic_Arm/src/main.cpp
+++ Robotic_Arm/src/main.cpp
@@ -1,81 +1,330 @@
+/****************************************************
+FILE:  AMS_5600_example
+
+Author: Tom Denton
+www.ams.com
+Date: 15 Dec 2014
+Version 1.00
+
+Description:  AS5600 "Potuino" demonstration application
+
+AMS5600 Programming Sketch
+***************************************************/
+
+#include <Wire.h>
 #include <Arduino.h>
+#include "AS5600.h"
 
-// --- Configuração dos motores (6 motores) ---
+#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
+  #define SYS_VOL   3.3
+#else
+  #define SYS_VOL   5
+#endif
 
-const int stepPins[6] = {2, 5, 8, 11, 14, 17};
-const int dirPins[6]  = {3, 6, 9, 12, 15, 18};
-const int enPins[6]   = {4, 7, 10, 13, 16, 19};
+String lastResponse;
+String noMagnetStr = "Error: magnet not detected";
 
-const float stepsPerRev = 200;     // full steps
-const int microsteps = 16;
-const float reductionRatio = 12.15;
-const float stepsPerDegree = (stepsPerRev * microsteps * reductionRatio) / 360.0;
+#define SDA_PIN 14
+#define SCL_PIN 15
 
-float currentAngles[6] = {0};  // Guarda o ângulo atual de cada motor
+TwoWire myWire(SDA_PIN, SCL_PIN);
+AMS_5600 ams5600(myWire);
 
-void setupMotor(int i) {
-  pinMode(stepPins[i], OUTPUT);
-  pinMode(dirPins[i], OUTPUT);
-  pinMode(enPins[i], OUTPUT);
-  digitalWrite(enPins[i], LOW); // Ativa driver
-}
-
-void moveToAngle(int motorIndex, float targetAngle) {
-  float delta = targetAngle - currentAngles[motorIndex];
-  bool direction = delta >= 0;
-  int steps = abs(delta * stepsPerDegree);
-
-  digitalWrite(dirPins[motorIndex], direction ? HIGH : LOW);
-
-  for (int i = 0; i < steps; i++) {
-    digitalWrite(stepPins[motorIndex], HIGH);
-    delayMicroseconds(100);
-    digitalWrite(stepPins[motorIndex], LOW);
-    delayMicroseconds(100);
+/*******************************************************
+* function: printMenu
+* In: none
+* Out: none
+* Description: prints menu options and result of last
+* command
+*******************************************************/
+void printMenu()
+{
+  Serial.println("AS5600 Serial Interface Program");
+  Serial.println("");
+  if(lastResponse.length()>0)
+  {
+    Serial.println(lastResponse);
+    Serial.println("");
   }
-
-  currentAngles[motorIndex] = targetAngle;
+  Serial.print("1 - Set start position\t|  "); Serial.println(" 7 - get raw angle");
+  Serial.print("2 - Set end position\t|  ");   Serial.println(" 8 - get scaled angle");
+  Serial.print("3 - Set max angle range\t|  ");  Serial.println(" 9 - detect magnet");
+  Serial.print("4 - Get max angle range\t|  ");  Serial.println("10 - get magnet strength");
+  Serial.print("5 - Get start position \t|  ");     Serial.println("11 - get automatic gain conrol");
+  Serial.println("6 - get end position \t|  ");
+  Serial.println();
+  // Serial.print("Number of burns remaining: "); Serial.println(String(3 - ams5600.getBurnCount()));
+  Serial.println("96 - Burn Angle");
+  Serial.println("98 - Burn Settings (one time)");
 }
 
-void setup() {
+/*******************************************************
+* Function: convertRawAngleToDegrees
+* In: angle data from AMS_5600::getRawAngle
+* Out: human readable degrees as float
+* Description: takes the raw angle and calculates
+* float value in degrees.
+*******************************************************/
+float convertRawAngleToDegrees(word newAngle)
+{
+  /* Raw data reports 0 - 4095 segments, which is 0.087 of a degree */
+  float retVal = newAngle * 0.087890625;
+  return retVal;
+}
+
+/*******************************************************
+* Function: convertScaledAngleToDegrees
+* In: angle data from AMS_5600::getScaledAngle
+* Out: human readable degrees as float
+* Description: takes the scaled angle and calculates
+* float value in degrees.
+*******************************************************/
+float convertScaledAngleToDegrees(word newAngle)
+{
+  word startPos = ams5600.getStartPosition();
+  word endPos = ams5600.getEndPosition();
+  word maxAngle = ams5600.getMaxAngle();
+
+  float multipler = 0;
+
+  /* max angle and end position are mutually exclusive*/
+  if(maxAngle >0)
+  {
+    if(startPos == 0)
+      multipler = (maxAngle*0.087890625)/4096;
+    else  /*startPos is set to something*/
+      multipler = ((maxAngle*0.087890625)-(startPos * 0.087890625))/4096;
+  }
+  else
+  {
+    if((startPos == 0) && (endPos == 0))
+      multipler = 0.087890625;
+    else if ((startPos > 0 ) && (endPos == 0))
+      multipler = ((360 * 0.087890625) - (startPos * 0.087890625)) / 4096;
+    else if ((startPos == 0 ) && (endPos > 0))
+      multipler = (endPos*0.087890625) / 4096;
+    else if ((startPos > 0 ) && (endPos > 0))
+      multipler = ((endPos*0.087890625)-(startPos * 0.087890625))/ 4096;
+  }
+  return (newAngle * multipler);
+}
+
+/*******************************************************
+* Function: burnAngle
+* In: none
+* Out: human readable string of success or failure
+* Description: attempts to burn angle data to AMS5600
+*******************************************************/
+String burnAngle()
+{
+  int burnResult = ams5600.burnAngle();
+  String returnStr = "Burn angle error: ";
+
+  switch (burnResult)
+  {
+    case 1:
+      returnStr = "Burn angle success";
+      break;
+    case -1:
+      returnStr += "no magnet detected";
+      break;
+    case -2:
+      returnStr += "no more burns left";
+      break;
+    case -3:
+      returnStr += "no positions set";
+      break;
+    default:
+      returnStr += "unknown";
+      break;
+  }
+  return returnStr;
+}
+
+/*******************************************************
+* Function: burnMaxAngleAndConfig
+* In: none
+* Out: human readable string of sucess or failure
+* Description: attempts to burn max angle and config data
+* to AMS5600
+*******************************************************/
+String burnMaxAngleAndConfig()
+{
+  int burnResult = ams5600.burnMaxAngleAndConfig();
+  String retStr = "Burn max angle and config error: ";
+
+  switch(burnResult)
+  {
+    case 1:
+      retStr = "Burn max angle and config success";
+      break;
+    case -1:
+      retStr += "chip has been burned once already";
+      break;
+    case -2:
+      retStr += "max angle less than 18 degrees";
+      break;
+    default:
+      retStr += "unknown";
+      break;
+  }
+  return retStr;
+}
+
+/*******************************************************
+* function: setup
+* In: none
+* Out: none
+* Description: called by system at startup
+*******************************************************/
+void setup(){
   Serial.begin(115200);
-  for (int i = 0; i < 6; i++) {
-    setupMotor(i);
+  while (!Serial);  // Importante para placas USB
+  myWire.begin();
+
+  delay(1000);
+  myWire.beginTransmission(0x36); // Endereço do AMS5600
+  if (myWire.endTransmission() == 0) {
+    Serial.println("AMS5600 detectado!");
+  } else {
+    Serial.println("AMS5600 NÃO detectado!");
   }
-  Serial.println("Sistema pronto para receber comandos.");
+  delay(1000);
+  printMenu();
 }
 
-void loop() {
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
-    input.trim();
 
-    if (input.startsWith("M")) {
-      int motorIndex = input.substring(1, 2).toInt() - 1;
-      float angle = input.substring(3).toFloat();
-      if (motorIndex >= 0 && motorIndex < 6) {
-        Serial.print("Movendo motor ");
-        Serial.print(motorIndex + 1);
-        Serial.print(" para ");
-        Serial.print(angle);
-        Serial.println(" graus");
-        moveToAngle(motorIndex, angle);
-        Serial.println("OK");
-      } else {
-        Serial.println("Motor inválido");
-      }
-    } else if (input.startsWith("J")) {
-      input.remove(0, 2);  // remove "J "
-      for (int i = 0; i < 6; i++) {
-        int spaceIndex = input.indexOf(' ');
-        String angleStr = (spaceIndex == -1) ? input : input.substring(0, spaceIndex);
-        float angle = angleStr.toFloat();
-        moveToAngle(i, angle);
-        input = (spaceIndex == -1) ? "" : input.substring(spaceIndex + 1);
-      }
-      Serial.println("OK");
-    } else {
-      Serial.println("Comando desconhecido.");
+ /*******************************************************
+  * Function: loop
+  * In: none
+  * Out: none
+  * Description: main program loop
+  *******************************************************/
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    char incomingByteBuffer[2] = {0};
+    int incomingByte = 0;
+
+    // Lê dois bytes da serial
+    Serial.readBytes(incomingByteBuffer, 2);
+
+    Serial.print("Recebido: ");
+    Serial.print(incomingByteBuffer[0]);
+    Serial.print(" ");
+    Serial.println(incomingByteBuffer[1]);
+
+    // Converte ASCII ('0'-'9') para número decimal
+    if (incomingByteBuffer[0] >= '0' && incomingByteBuffer[0] <= '9')
+    {
+      incomingByte = (incomingByteBuffer[0] - '0') * 10;
     }
+
+    if (incomingByteBuffer[1] >= '0' && incomingByteBuffer[1] <= '9')
+    {
+      incomingByte += (incomingByteBuffer[1] - '0');
+    }
+
+    Serial.print("Comando interpretado: ");
+    Serial.println(incomingByte);
+
+    // Executa a ação correspondente
+    switch (incomingByte)
+    {
+      case 1:
+        Serial.println("Ação: Set Start Angle");
+        lastResponse = ams5600.detectMagnet() ?
+          "Start angle set to = " + String(convertRawAngleToDegrees(ams5600.setStartPosition()), DEC) :
+          noMagnetStr;
+        break;
+
+      case 2:
+        Serial.println("Ação: Set End Angle");
+        lastResponse = ams5600.detectMagnet() ?
+          "End angle set to = " + String(convertRawAngleToDegrees(ams5600.setEndPosition()), DEC) :
+          noMagnetStr;
+        break;
+
+      case 3:
+        Serial.println("Ação: Set Max Angle");
+        lastResponse = ams5600.detectMagnet() ?
+          "Max angle range set to = " + String(convertRawAngleToDegrees(ams5600.setMaxAngle()), DEC) :
+          noMagnetStr;
+        break;
+
+      case 4:
+        Serial.println("Ação: Get Max Angle");
+        lastResponse = "Max angle range = " + String(convertRawAngleToDegrees(ams5600.getMaxAngle()), DEC);
+        break;
+
+      case 5:
+        Serial.println("Ação: Get Start Angle");
+        lastResponse = "Start angle = " + String(convertRawAngleToDegrees(ams5600.getStartPosition()), DEC);
+        break;
+
+      case 6:
+        Serial.println("Ação: Get End Angle");
+        lastResponse = "End angle = " + String(convertRawAngleToDegrees(ams5600.getEndPosition()), DEC);
+        break;
+
+      case 7:
+        Serial.println("Ação: Get Raw Angle");
+        lastResponse = "Raw angle = " + String(convertRawAngleToDegrees(ams5600.getRawAngle()), DEC);
+        break;
+
+      case 8:
+        Serial.println("Ação: Get Scaled Angle");
+        lastResponse = "Scaled angle = " + String(convertScaledAngleToDegrees(ams5600.getScaledAngle()), DEC);
+        break;
+
+      case 9:
+        Serial.println("Ação: Detect Magnet");
+        lastResponse = ams5600.detectMagnet() ? "Magnet detected" : noMagnetStr;
+        break;
+
+      case 10:
+        Serial.println("Ação: Get Magnet Strength");
+        if (ams5600.detectMagnet())
+        {
+          int magStrength = ams5600.getMagnetStrength();
+          lastResponse = "Magnet strength ";
+          if (magStrength == 1)
+            lastResponse += "is weak";
+          else if (magStrength == 2)
+            lastResponse += "is acceptable. Current Magnitude: " + String(ams5600.getMagnitude());
+          else if (magStrength == 3)
+            lastResponse += "is too strong";
+        }
+        else
+        {
+          lastResponse = noMagnetStr;
+        }
+        break;
+
+      case 11:
+        Serial.println("Ação: Get AGC");
+        lastResponse = "Automatic Gain Control = " + String(ams5600.getAgc(), DEC);
+        break;
+
+      case 96:
+        Serial.println("Ação: Burn Angle");
+        lastResponse = burnAngle();
+        break;
+
+      case 98:
+        Serial.println("Ação: Burn Max Angle and Config");
+        lastResponse = burnMaxAngleAndConfig();
+        break;
+
+      default:
+        Serial.println("Ação: Comando inválido");
+        lastResponse = "Invalid Entry";
+        break;
+    }
+
+    // Mostra resposta e menu novamente
+    Serial.println("Resposta: " + lastResponse);
+    printMenu();
   }
 }
